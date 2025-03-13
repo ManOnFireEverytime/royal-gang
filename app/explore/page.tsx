1fff45664b31eeeb3efa35fdfed9aee4b0a6568f
Mofe Ogunkanmi <manonfireeverytime@gmail.com>
2025-03-13 22:04:56 +0000

    product page

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,38 +1,58 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
 import Card from "../_components/Card";
 import PageWrapper from "../_components/PageWrapper";
-
 import embraceTshirt from "@/public/embrace.webp";
 import signatureTank from "@/public/signatureTank.webp";
 
-//TODO: IF NOT USING SERVER ACTIONS, CHANGE TO CLIENT SIDE RENDERING
+// Define the Product interface
+interface Product {
+  id: string;
+  // Add other product properties as needed
+}
+
 export default function ExploreAll() {
+  const { data: products, status, error } = useQuery({
+    queryKey: ["products"],
+    queryFn: async function (): Promise<Product[]> {
+      const response = await fetch(
+        "https://backend.royalgangchambers.com/getAllProducts.php",
+      );
+      
+      if (!response.ok) {
+        throw new Error("Failed to fetch products.");
+      }
+      
+      const data = await response.json();
+      
+      return data.data;
+    },
+  });
+
   return (
     <PageWrapper>
       <h1 className="py-4 text-center font-bold">ALL PRODUCTS</h1>
-
-      {/*TODO: fetch all the producta to be displayed and replace fake array with the array of products */}
-      <div className="grid grid-cols-2 place-items-center items-center justify-between lg:grid-cols-4">
-        {Array.from({ length: 20 }).map((c, i) => (
-          <Card
-            productInfo={{
-              id: "22",
-              product_name: "VICTORY’S EMBRACE T SHIRT",
-              price: "54640",
-              image1: i % 2 ? signatureTank.src : embraceTshirt.src,
-              description: "",
-              colors: "",
-              sizes: "",
-              image2: "",
-              image3: "",
-              image4: "",
-              image5: "",
-              created_at: "",
-              category: "",
-            }}
-            key={i}
-          />
-        ))}
-      </div>
+      
+      {status === "loading" && (
+        <div className="flex justify-center py-10">
+          <p>Loading products...</p>
+        </div>
+      )}
+      
+      {status === "error" && (
+        <div className="flex justify-center py-10">
+          <p>Error loading products: {error?.message}</p>
+        </div>
+      )}
+      
+      {status === "success" && (
+        <div className="grid grid-cols-2 place-items-center items-center justify-between lg:grid-cols-4">
+          {products.map((product) => (
+            <Card key={product.id} productInfo={product} />
+          ))}
+        </div>
+      )}
     </PageWrapper>
   );
 }
